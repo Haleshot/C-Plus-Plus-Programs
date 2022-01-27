0a8db7197bfeddd284426df4659eb7e18d436b72
Haleshot <57552973+LeoTeo2828@users.noreply.github.com>
2022-01-27 22:29:30 +0530

    Structure type comment on top

--- 2ndSem_Practical_Programs/Exp_1/Task_3/Task3_Strcuture_Type.cpp
+++ 2ndSem_Practical_Programs/Exp_1/Task_3/Task3_Strcuture_Type.cpp
@@ -3,7 +3,9 @@ Task 3 done with functions
 The function call in int main don't seem to work when rows and columns are passed but when integer is used, they work.
 Example when Image_Matrix[row][column] is defined, calling GrayScaleImage(Image_Matrix) doesn't work
 But when Image_Matrix[3][3] is defined, GrayScaleImage(Image_Matrix); works. 
-Try using this to see what causes the error*/
+Try using this to see what causes the error
+Reference Website - https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function
+*/
 #include <iostream>
 using namespace std;
 
