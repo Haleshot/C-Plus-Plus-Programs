92bf2a6b87128b5531def4ab40412705e446e4d0
Haleshot <hari.leo03@gmail.com>
2022-01-28 21:02:24 +0530

    Updated Code for 3x3 Matrix

--- 2ndSem_Practical_Programs/Exp_1/Task_3/Task3_Strcuture_Type.cpp
+++ 2ndSem_Practical_Programs/Exp_1/Task_3/Task3_Strcuture_Type.cpp
@@ -29,7 +29,6 @@ void GrayScaleImage(int (&a)[rows][columns])
             a[i][j] = {0};
         }
     }
-    DisplayScaleImage(a);
 }
 
 template <int rows, int columns>
@@ -67,23 +66,18 @@ void clear_value(int (&a)[rows][columns])
             a[i][j] = {value_1};
         }
     }
-    DisplayScaleImage(a);
 }
 
 template <int rows, int columns>
-void get_item(int (&a)[rows][columns])
+void get_item(int (&a)[rows][columns], int enter_row, int enter_column)
 {
-    cout << "Enter Height and Width to find Intensity value : " << endl;
-    cin >> rows >> columns;
-    cout << a[rows][columns];
+    cout << a[enter_row - 1][enter_column - 1];
 }
 
 template <int rows, int columns>
-void set_item(int (&a)[rows][columns])
+void set_item(int (&a)[rows][columns], int enter_row, int enter_column)
 {
     int value_2;
-    cout << "Enter Height and Width whose Intensity value is to be changed :" << endl;
-    cin >> rows >> columns;
 
     cout << "Enter Value : ";
     cin >> value_2;
@@ -96,8 +90,7 @@ void set_item(int (&a)[rows][columns])
     {
         value_2 = 0;
     }
-    a[rows][columns] = {value_2};
-    DisplayScaleImage(a);
+    a[enter_row][enter_column] = {value_2};
 }
 
 
@@ -111,41 +104,70 @@ int main()
     cin >> row;
     cout << "Enter Limit for Width(Column of matrix)  of Image : ";
     cin >> column;
+    int Image_Matrix[3][3];
 
-    cout << "Menu\n 1. Creation of a 2D array which consists of n rows x n columns \n 2. Returns the width of the image. \n 3. Returns the height of the image. \n 4. Clears the entire image by setting each pixel to the entered intensity value(Please enter value between 0 and 255). \n 5. Returns the intensity level of the given pixel. \n 6. Sets the intensity level of the given pixel to the given value(Please enter value between 0 and 255). \n" ;
-    cout << "Enter Choice\n" ;
-    cin >> ch;
-    int Image_Matrix[row][column];
     do
     {
+        cout << "Menu\n 1. Creation of a 2D array which consists of n rows x n columns \n  2. Display 2D array \n 3. Returns the width of the image. \n 4. Returns the height of the image. \n 5. Clears the entire image by setting each pixel to the entered intensity value(Please enter value between 0 and 255). \n 6. Returns the intensity level of the given pixel. \n 7. Sets the intensity level of the given pixel to the given value(Please enter value between 0 and 255). \n" ;
+        cout << "Enter Choice\n" ;
+        cin >> ch;
         if(ch == 1)
         {
             GrayScaleImage(Image_Matrix);
             DisplayScaleImage(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
         else if(ch == 2)
         {
-            Return_Width(Image_Matrix);
+            DisplayScaleImage(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
         else if(ch == 3)
         {
-            Return_Height(Image_Matrix);
+            Return_Width(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
         else if(ch == 4)
         {
-            clear_value(Image_Matrix);
-            DisplayScaleImage(Image_Matrix);
+            Return_Height(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
         else if(ch == 5)
         {
-            get_item(Image_Matrix);
+            clear_value(Image_Matrix);
+            DisplayScaleImage(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
         else if(ch == 6)
         {
-            set_item(Image_Matrix);
+            int row_value_1, column_value_1;
+            cout << "Enter Limit for Height(Row of matrix) of Image : ";
+            cin >> row_value_1;
+            cout << "Enter Limit for Width(Column of matrix)  of Image : ";
+            cin >> column_value_1;
+            cout << endl;
+            get_item(Image_Matrix, row_value_1, column_value_1);
+
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        else if(ch == 7)
+        {
+            int row_value_2, column_value_2;
+            cout << "Enter Limit for Height(Row of matrix) of Image : ";
+            cin >> row_value_2;
+            cout << "Enter Limit for Width(Column of matrix)  of Image : ";
+            cin >> column_value_2;
+            set_item(Image_Matrix, row_value_2, column_value_2);
             DisplayScaleImage(Image_Matrix);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
         }
     } while (run == true);
-
     return 0;
 }
