b738ae30f551229629a30284b7ba999a2e6dc46a
Haleshot <hari.leo03@gmail.com>
2022-01-29 11:50:09 +0530

    Modified

--- 2ndSem_Practical_Programs/Exp_1/Task_2/main.cpp
+++ 2ndSem_Practical_Programs/Exp_1/Task_2/main.cpp
@@ -33,6 +33,69 @@ void High_Low(int A[], int n)
     cout << "Max of Array Elements is = " << max << endl;
     cout << "Min of Array Elements is = " << min << endl;
 }
+void Insert(int A[], int n, int values, int indices)
+{
+    int i;
+    
+    if(indices > n)
+    {
+        cout << "Index value greater than array limit" << endl;
+    }
+    else if(indices < n)
+    {
+
+        cout << "Elements of Array before Insertion :" << endl;
+        for(i = 0; i < n; i++)
+        {
+            cout << A[i] << " ";
+        }
+
+        for(i = n - 1; i > indices - 1; i--)
+        {
+            A[i + 1] = A[i];
+        }
+
+         A[indices] = values;
+
+        cout << "\nElements of Array after Insertion :" << endl;
+        for(i = 0; i <= n; i++)
+        {
+            cout << A[i] << " ";
+        }
+    }
+
+}
+
+void Delete(int A[], int n, int indices)
+{
+    int i = 0;
+    
+    if(indices > n)
+    {
+        cout << "Index value greater than array limit" << endl;
+    }
+    else if(indices < n)
+    {
+        cout << "Elements of Array before Deletion :" << endl;
+        for(i = 0; i < n; i++)
+        {
+            cout << A[i] << " ";
+        }
+
+        for(i = indices; i < n - 1; i++)
+        {
+            A[i] = A[i + 1];
+        }
+    
+        cout << "\nElements of Array after Deletion :" << endl;
+        for(i = 0; i < n - 1; i++)
+        {
+            cout << A[i] << " ";
+        }
+    }
+
+
+}
 
 int main()
 {
@@ -52,21 +115,40 @@ int main()
             cin >> A[i];
         }
 
-        cout << "Menu\n 1. Sum and Average of Array Elements, \n 2. Maximum and Minimum of Array Elements\n" ;
+        cout << "Menu\n 1. Sum and Average of Array Elements\n 2. Maximum and Minimum of Array Elements\n 3. Insert Element\n 4. Delete element\n" ;
         cout << "Enter Choice\n" ;
         cin >> ch;
         switch(ch)
         {
             case 1:
                 Sum_Avg(A, limit);
-                cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+                cout << "\nWant to continue? (Yes = Input 1/false = Input 0) : " << endl;
                 cin >> run;
                 break;
             case 2:
                 High_Low(A, limit);
-                cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+                cout << "\nWant to continue? (Yes = Input 1/false = Input 0) : " << endl;
                 cin >> run;
                 break;
+            case 3:
+                int value, index_1;
+                cout << "Enter the Index Position : ";
+                cin >> index_1;
+                cout << "Enter the Value : ";
+                cin >> value;
+                Insert(A, limit, value, index_1);
+                cout << "\nWant to continue? (Yes = Input 1/false = Input 0) : " << endl;
+                cin >> run;
+                break;
+            case 4:
+                int index_2;
+                cout << "Enter the Index Position : ";
+                cin >> index_2;
+                Delete(A, limit, index_2);
+                cout << "\nWant to continue? (Yes = Input 1/false = Input 0) : " << endl;
+                cin >> run;
+                break;
+
 
             default :
 
