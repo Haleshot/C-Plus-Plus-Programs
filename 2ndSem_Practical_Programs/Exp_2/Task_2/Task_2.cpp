91d6a3357c0a472d004487a0068ee48f4187d784
Haleshot <hari.leo03@gmail.com>
2022-02-08 21:19:46 +0530

    Updating Experiment_2 Tasks

---
+++ 2ndSem_Practical_Programs/Exp_2/Task_2/Task_2.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+using namespace std;
+#define Max 100
+int Stack[Max], top = -1;
+
+void Push(int top_value)
+{
+    if (top_value == Max - 1)
+    {
+        cout << "Overflow!" << endl;
+    }
+    else
+    {
+        top_value += 1;
+        Stack[top] = top_value;
+    }
+}
+
+int Pop()
+{
+    if (top < -1)
+    {
+        cout << "Underflow!" << endl;
+    }
+    else
+    {
+        int x = Stack[top];
+        top -= 1;
+        return x;
+    }
+}
+
+int main()
+{
+    int ch, i;
+    bool run = true;
+    char expression_array[100];
+    for(int i = 0; i < 100; i++)
+    {
+        expression_array[i] = 0;
+    }
+
+    do while (run == true)
+    {
+        string expression;
+        cout << "Enter the postfix expression to be evaluated : ";
+        int length = sizeof(expression);
+        char expression_array[length];
+        for(int i = 0; i < length; i++)
+        {
+            expression_array[i] = expression[i];
+        }
+
+        for(i = 0; i < length; i++)
+        {
+            int A, B, C;
+            switch (expression_array[i])
+            {
+                    case '+':
+                        A = Pop();
+                        B = Pop();
+                        C = B + A;
+                        Push(C);
+                        break;
+
+                    case '-':
+                        A = Pop();
+                        B = Pop();
+                        C = B - A;
+                        Push(C);
+                        break;
+
+                    case '*':
+                        A = Pop();
+                        B = Pop();
+                        C = B * A;
+                        Push(C);
+                        break;
+
+                    case '/':
+                        A = Pop();
+                        B = Pop();
+                        C = B / A;
+                        Push(C);
+                        break;
+
+                    case '^':
+                        A = Pop();
+                        B = Pop();
+                        C = pow(B, A);
+                        Push(C);
+                        break;
+                        
+                    default:
+                        Push(expression_array[i] - '0');
+            }
+        }
+
+        cout << "The Value of the expression " << expression << " is = " << expression_array[0] << endl;
+        
+        cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+        cin >> run;
+
+    }while (run == true);
+    
+    
+    return 0;
+
+}
