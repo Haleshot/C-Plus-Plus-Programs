8066861c1bf70d06e8b9cbbf18c59805e2fc393d
Haleshot <hari.leo03@gmail.com>
2022-03-21 08:45:06 +0530

    Updated

--- Codechef_Programs/Assignment_Programs/Week_4/Task_1.cpp
+++ Codechef_Programs/Assignment_Programs/Week_4/Task_1.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    int T;
+    cout << "Enter the Number of Test Cases" << endl;
+    cin >> T;
+    int list[10], counter[10];
+
+
+    for(int i = 0; i < T; i++)
+    {
+        int n;
+        cout << "Enter the Number of Students" << endl;
+        cin >> n;
+        counter[i] = n;
+        char exp[n];
+        cin >> exp;
+        list[n];
+        
+
+        for(int j = 0; j < n; j++)
+        {
+            if(isdigit(exp[j]))
+            {
+                list[j] = int(exp[j]);
+            }
+        }
+    }
+
     return 0;
 }
