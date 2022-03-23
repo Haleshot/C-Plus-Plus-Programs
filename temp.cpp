9f0abf4d61827dc9178756766d342e4923dc679f
Haleshot <hari.leo03@gmail.com>
2022-03-23 17:26:06 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define MAX 100
 int Stack[MAX], top = -1;
 
-void Push(int x)
+void Push(char x)
 {
-    if (top == MAX - 1)
+    if(top > MAX - 1)
     {
         cout << "Overflow!" << endl;
     }
@@ -18,88 +18,23 @@ void Push(int x)
 
 int Pop()
 {
-    if(top == -1)
+    if(top < 0)
     {
         cout << "Underflow!" << endl;
     }
     else
     {
-        int x = Stack[top];
+        int last = Stack[top];
         top -= 1;
-        return x;
+        return last;
     }
 }
 
+
+
+
 int main()
 {
-    bool run = true;
-    for(int i = 0; i < MAX; i++)
-    {
-        Stack[i] = 0;
-    }
-    do
-    {
-        top = -1;
-        string exp = "";
-        cout << "Enter the Postfix Expression\n";
-        cin >> exp;
-        int length = exp.length();
-        Stack[length];
 
-        for(int i = 0; i < MAX; i++)
-         {
-            Stack[i] = exp[i];
-        }
-        for(int i = 0; i < length; i++)
-        {
-            int A, B, C;
-            switch(Stack[i])
-            {
-                case '+':
-                    A = Pop();
-                    B = Pop();
-                    C = B + A;
-                    Push(C);
-                    break;
-
-                case '-':
-                    A = Pop();
-                    B = Pop();
-                    C = B - A;
-                    Push(C);
-                    break;
-                
-                case '*':
-                    A = Pop();
-                    B = Pop();
-                    C = B * A;
-                    Push(C);
-                    break;
-
-                case '/':
-                    A = Pop();
-                    B = Pop();
-                    C = B / A;
-                    Push(C);
-                    break;
-                
-                case '^':
-                    A = Pop();
-                    B = Pop();
-                    C = B ^ A;
-                    Push(C);
-                    break;
-                
-                default:
-                    Push(Stack[i] - '0');
-            }
-        }
-
-        cout << "The Value of the Postfix expression is = " << Stack[0];
-
-        cout << "\nWant to continue? (Yes = Input 1/false = Input 0) : " << endl;
-        cin >> run;
-
-    } while (run ==  true);
-    
+    return 0;
 }
