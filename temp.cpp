8569036fe63efb8335162d9b4fe59bf34af4a86c
Haleshot <hari.leo03@gmail.com>
2022-03-23 20:38:47 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -116,5 +116,127 @@ void Delete_Pos(int index)
         temp = temp->next;
         counter++;
     }
+    if(temp != NULL)
+    {
+        Node *a = temp->next;
+        Node *b = a->next;
+        temp->next = b;
+        delete a;
+    }
 
+}
+
+void Modify(int index, int value)
+{
+    Node *current = head;
+    int counter = 0;
+
+    if(head == NULL)
+    {
+        cout << "Linked List is Empty!" << endl;
+    }
+    while(current != NULL && counter < index - 1)
+    {
+        current = current->next;
+        counter++;
+    }
+    if(current != NULL)
+    {
+        current->data = value;
+    }
+}
+
+int main()
+{
+    int ch;
+    bool run = true;
+    do
+    {
+        cout << "Menu \n 1. Insert Value at the Beginning \n 2. Display Values \n 3. Insert Value at the End \n 4. Insert Value at a particular position \n 5. Modify value of Node at a position \n 6. Delete Value at a particular position \n 7. Delete Value at Beginning \n 8. Delete Value at the End \n 9. Display length of Linked List \n 10. Exit \n" ;
+        cout << "Enter Choice\n" ;
+        cin >> ch;
+
+        if (ch == 1)
+        {
+            int value_push;
+            cout << "Enter Value to be pushed into the Linked List : ";
+            cin >> value_push;
+            Insert_Beg(value_push);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if (ch == 2)
+        {
+
+            Display();
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if (ch == 3)
+        {
+            int value_push;
+            cout << "Enter Value to be pushed into the Linked List : ";
+            cin >> value_push;
+            Insert_End(value_push);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+
+        }
+
+        if( ch == 4)
+        {
+            int value_push, Index;
+            cout << "Enter Value and Index to be pushed into the Linked List : ";
+            cin >> value_push >> Index;
+            Insert_Pos(Index, value_push);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+
+        }
+        if (ch == 5)
+        {
+            int value_push, Index;
+            cout << "Enter Value and Index to be modified in the Linked List : ";
+            cin >> value_push >> Index;
+            Modify(Index, value_push);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if( ch == 6)
+        {
+            int Index;
+            cout << "Enter Index from which element must be deleted from the Linked List : ";
+            cin >> Index;
+            Delete_Pos(Index);
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if( ch == 7)
+        {
+            Delete_Beg();
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if( ch == 8)
+        {
+            Delete_End();
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if( ch == 9)
+        {
+            Delete_End();
+            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
+            cin >> run;
+        }
+        if ( ch == 10)
+        {
+            return 0;
+        }
+        
+        
+    } while(run == true);
+
+
+    return 0;
 }
