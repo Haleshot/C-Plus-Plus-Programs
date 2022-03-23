481ddc7bc93de2923d161de86c87b2127a693054
Haleshot <hari.leo03@gmail.com>
2022-03-23 20:00:39 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -20,12 +20,84 @@ void Display()
     }
 }
 
-void Insert_Beg()
+void Insert_Beg(int value)
 {
-    Node *first = head;
-    
-
+    Node *first;
+    first = new Node();
+    first->data = value;
+    first->next = head;
+    head = first;
 }
-void Insert_End();
-void Delete_Beg();
-void Delete_End();
+
+void Insert_End(int value)
+{
+    Node *last;
+    Node *current;
+    last = new Node();
+    last->data = value;
+    last->next = NULL;
+
+    if (head == NULL)
+    {
+        head = last;
+    }
+    else
+    {
+        while(current != NULL)
+        {
+            current = current->next;
+        }
+        current->next = last;
+    }
+}
+void Delete_Beg()
+{
+    if(head == NULL)
+    {
+        cout << "Linked List is Empty!" << endl;
+    }
+    else
+    {
+        Node *current;
+        current = head;
+        head = head->next;
+        delete current;
+    }
+}
+void Delete_End()
+{
+    Node *current = head;
+    Node *temp;
+    if(head == NULL)
+    {
+        cout << "Linked List is Empty!" << endl;
+    }
+    else
+    {
+        while(current != NULL)
+        {
+            temp = current;
+            current = current->next;
+        }
+        temp->next = NULL;
+        delete current;
+    }
+}
+
+void Insert_Pos(int index, int value)
+{
+    Node *current = head;
+    Node *temp = current;
+    Node *mid = new Node();
+
+    int counter = 1;
+    for(int i = 0; i < index; i++)
+    {
+        temp = current;
+        current = current->next;
+        counter++;
+    }
+    mid->data = value;
+    mid->next = current;
+    temp->next = current;
+}
