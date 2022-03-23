e113c3196889ab2c40b55014fcfe2affbb56b852
Haleshot <hari.leo03@gmail.com>
2022-03-23 19:34:58 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -1,145 +1,31 @@
 #include <iostream>
 using namespace std;
-#define MAX 100
-int Circular_Queue[MAX], front = -1, rear = -1;
 
-int IsEmpty()
+struct Node
 {
-    if(front == -1)
-    {
-        front = rear = 0;
-        return 1;
-    }
-    return 0;
-}
+    int data;
+    Node *next;
+};
 
-int IsFull()
-{
-    if((rear + 1) % MAX == front)
-    {
-        return 1;
-    }
-    return 0;
-}
+Node *head = NULL;
 
-void Enqueue(int value)
-{
-    if(IsEmpty())
-    {
-        Circular_Queue[rear] = value;
-    }
-    else if(IsFull())
-    {
-        cout << "Overflow!" << endl;
-        return;
-    }
-    else
-    {
-        rear = (rear + 1) % MAX;
-        Circular_Queue[rear] = value;
-    }
-}
-
-
-void Dequeue()
-{
-    if(IsEmpty())
-    {
-        cout << "Underflow!" << endl;
-        return;
-    }
-    else if(front == rear)
-    {
-        int x;
-        x = Circular_Queue[rear];
-        cout << "Popped out Element is = " << x << endl;
-        front = rear = -1;
-    }
-    else
-    {
-        int x;
-        x = Circular_Queue[rear];
-        cout << "Popped out Element is = " << x << endl;
-        front = (front + 1) % MAX;
-    }
-
-}
 
 void Display()
 {
-    if(front == -1)
+    Node *current = head;
+    while(current != NULL)
     {
-        cout << "Queue is Empty!" << endl;
-    }
-    else
-    {
-        cout <<"Queue Elements are :" << endl;
-        for(int i = front; i <= rear; i++)
-        {
-            cout << Circular_Queue[i] << endl;
-        }
+        cout << current->data;
+        current = current->next;
     }
 }
 
-void Peek()
+void Insert_Beg()
 {
-    if(front == -1)
-    {
-        cout << "Queue is Empty!" << endl;
-    }
-    else
-    {
-        cout << "Element present at Queue's rear is = " << Circular_Queue[rear] << endl;
-    }
+    Node *first = head;
+    
+
 }
-
-int main()
-{
-    bool run = true;
-    int ch;
-
-    do
-    {
-        cout << "Menu \n1. Enqueue Value\n2. Dequeue Value\n3. Peek Value\n4. Display Values\n5. Exit\n";
-        cout << "Enter your choice : " << endl;
-        cin >> ch;
-
-        if (ch == 1)
-        {
-            int value_push;
-            cout << "Enter Phone Number to be pushed into the Queue : ";
-            cin >> value_push;
-            Enqueue(value_push);
-            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
-            cin >> run;
-        }
-        
-        if (ch == 2)
-        {
-            Dequeue();
-            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
-            cin >> run;
-        }
-
-        if (ch == 3)
-        {
-            Peek();
-            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
-            cin >> run;
-        }
-
-        if (ch == 4)
-        {
-            Display();
-            cout << "Want to continue? (Yes = Input 1/false = Input 0) : " << endl;
-            cin >> run;
-        }
-
-        if (ch == 5)
-        {
-            return 0;
-        }
-        
-    } while (run == true);
-    return 0;
-}
+void Insert_End();
+void Delete_Beg();
+void Delete_End();
