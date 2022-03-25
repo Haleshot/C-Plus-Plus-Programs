97938dab92977ac78c650086980fb176ba103e52
Haleshot <hari.leo03@gmail.com>
2022-03-25 21:53:26 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -1,104 +1,87 @@
 #include <iostream>
 using namespace std;
 #define MAX 100
-int Character_Stack[MAX], top = -1;
-char x;
-
-int check_match(char x_, char y_)
-{
-    if(x_ == '(' && y_ == ')')
-    {
-        return 1;
-    }
-    if(x_ == '{' && y_ == '}')
-    {
-        return 1;
-    }
-    if(x_ == '[' && y_ == ']')
-    {
-        return 1;
-    }
-    return 0;
-}
+int Circular_Queue[MAX], front = -1, rear = -1;
 
 int IsEmpty()
 {
-    if(top < 0)
+    if(front == -1)
     {
+        front = rear = 0;
         return 1;
     }
-    else
-    {
-        return 0;
-    }
 }
 
-int Pop()
+int IsFull()
 {
-    if(top < 0)
+    if((rear + 1) % MAX == front)
     {
-        cout << "Underflow!" << endl;
-    }
-    else
-    {
-        int x = Character_Stack[top];
-        top -= 1;
-        return x;
-    }
-}
-
-void Push(char value)
-{
-    if(top > MAX - 1)
-    {
-        cout << "Overflow!" << endl;
-    }
-    else
-    {
-        top += 1;
-        Character_Stack[top] = value;
-    }
-}
-
-
-bool Check_Balance_Parantheses(string expression)
-{
-    for(int i = 0; i < expression.length(); i++)
-    {
-        if(expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
-        {
-            Push(expression[i]);
-        }
-        else if(expression[i] == ')' || expression[i] == '}' || expression[i] == ']')
-        {
-                char top_value = Character_Stack[top];
-                if(IsEmpty() || check_match(top_value, expression[i]) == 0)
-                {
-                    return 0;
-                }
-                else
-                {
-                    x = Pop();
-                }
-            }
-    }
-    return(IsEmpty());
-}
-
-
-int main()
-{
-    string sentence;
-    cin >> sentence;
-    if(Check_Balance_Parantheses(sentence) == 1)
-    {
-        cout << "Balanced!" << endl;
-    }
-    else if(Check_Balance_Parantheses(sentence) == 0)
-    {
-        cout << "Not Balanced!" << endl;
+        return 1;
     }
     return 0;
 }
 
+void Enqueue(int value)
+{
+    if(IsEmpty())
+    {
+        Circular_Queue[rear] = value;
+    }
+    else if(IsFull())
+    {
+        cout << "Queue is Full!" << endl;
+    }
+    else
+    {
+        rear = (rear + 1) % MAX;
+        Circular_Queue[rear] = value;
+    }
+}
 
+int Dequeue()
+{
+    if(IsEmpty())
+    {
+        cout << "Queue is Empty!" << endl;
+    }
+    else if(front == rear)
+    {
+        int x = Circular_Queue[front];
+        cout << "Popped out Element is = " << x << endl;
+        front = rear = -1;
+    }
+    else
+    {
+        int x = Circular_Queue[front];
+        cout << "Popped out Element is = " << x << endl;
+        front = (front + 1) % MAX;
+    }
+}
+
+void Peek()
+{
+    if(front < 0)
+    {
+        cout << "Queue is Empty!" << endl;
+    }
+    else
+    {
+        cout << "Element present at Queue's front is = " << Circular_Queue[front] << endl;
+    }
+}
+
+
+void Display()
+{
+    if(front < 0)
+    {
+        cout << "Queue is Empty!" << endl;
+    }
+    else
+    {
+        for(int i = front; i <= rear; i++)
+        {
+            cout << Circular_Queue[i] << endl;
+        }
+    }
+}
