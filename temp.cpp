f5f30db966f9955409444edee89f082d32d2eb62
Haleshot <hari.leo03@gmail.com>
2022-03-25 21:19:52 +0530

    Updated

--- temp.cpp
+++ temp.cpp
@@ -1,58 +1,5 @@
 #include <iostream>
 using namespace std;
 #define MAX 100
-int Stack[MAX], top = -1;
+int Character_Stack[MAX], top = -1;
 
-void Push(int value)
-{
-    if(top > MAX - 1)
-    {
-        cout << "Overflow!" << endl;
-    }
-    else
-    {
-        top += 1;
-        Stack[top] = value;
-    }
-}
-
-void Pop()
-{
-    if(top < 0)
-    {
-        cout << "Underflow!" << endl;
-    }
-    else
-    {
-        int x = Stack[top];
-        cout << x;
-        top -= 1;
-    }
-}
-
-
-int main()
-{
-    bool run = true;
-    for(int i = 0; i < MAX; i++)
-        {
-            Stack[i] = 0;
-        }
-    do
-    {
-        top = -1;
-        string exp;
-        cout << "Enter the expression" << endl;
-        cin >> exp;
-        int length = exp.length();
-        Stack[length];
-
-        for(int i = 0; i < length; i++)
-        {
-            Stack[i] = exp[i];
-        }
-        
-    } while (run == true);
-    
-    return 0;
-}
