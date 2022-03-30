c3f31b5ef8007cfc9f237fe9562a212761db74cb
Haleshot <hari.leo03@gmail.com>
2022-03-30 20:12:14 +0530

    Updated

--- 2nd_sem_DSA_Project/Project.cpp
+++ 2nd_sem_DSA_Project/Project.cpp
@@ -61,14 +61,77 @@ class Restaurants
 private:
     /* data */
 public:
-    void Show_nearby_restaurants()
-    {
-        cout << "The List of Veg Restaurants is:\n1.Mia Cucina\n2.Bayroute\n3.Tiara\n4.Parabola\n5.Pizza Express\n6.Mainland China\n";
-        cout << "The List of Non-Veg Restaurants is:\n1.Sigree Global Grill\n2.The Fatty Bao\n3.Ticca Tikka\n4.Global Fusion\n5.Kofuku\n6.Shizusan\n";
-        
-    }
-    
+    void Show_nearby_restaurants();
+    void Mia_Cucina();
+    void Bayroute();
+    void Tiara();
+    void Parabola();
+    void Pizza_Express();
+    void Mainland_China();
+    void Sigree_Global_Grill();
+    void The_Fatty_Bao();
+    void Ticca_Tikka();
+    void Global_Fusion();
+    void Kofuku();
+    void Shizusan();
 };
+
+
+void Restaurants::Show_nearby_restaurants()
+{
+    cout << "The List of Veg Restaurants is:\n1.Mia Cucina\n2.Bayroute\n3.Tiara\n4.Parabola\n5.Pizza Express\n6.Mainland China\n";
+    cout << "The List of Non-Veg Restaurants is:\n1.Sigree Global Grill\n2.The Fatty Bao\n3.Ticca Tikka\n4.Global Fusion\n5.Kofuku\n6.Shizusan\n";
+        
+}
+void Restaurants::Mia_Cucina()
+{
+    cout << "The List of Veg Restaurants is: " << endl;
+}
+void Restaurants::Bayroute()
+{
+
+}
+void Restaurants::Tiara()
+{
+    
+}
+void Restaurants::Parabola()
+{
+    
+}
+void Restaurants::Pizza_Express()
+{
+    
+}
+void Restaurants::Mainland_China()
+{
+    
+}
+void Restaurants::Sigree_Global_Grill()
+{
+    
+}
+void Restaurants::The_Fatty_Bao()
+{
+    
+}
+void Restaurants::Ticca_Tikka()
+{
+    
+}
+void Restaurants::Global_Fusion()
+{
+    
+}
+void Restaurants::Kofuku()
+{
+    
+}
+void Restaurants::Shizusan()
+{
+    
+}
+
 
 
 
