5146151ff152fe13e67b9fab85cb07182bd6ca8d
Haleshot <hari.leo03@gmail.com>
2022-04-16 10:27:29 +0530

    Updated

---
+++ 2ndSem_Practical_Programs/Exp_8/Binary_Search_Tree.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+Node *insertBST(Node *root, int value)
+{
+    if(root == NULL)
+    {
+        Node *newNode = new Node;
+        newNode->data = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        root = newNode;
+        
+    }
+    
+    else if(value < root->data)
+    {
+        root->left = insertBST(root->left, value);
+    }
+
+    else if(value > root->data)
+    {
+        root->right = insertBST(root->right, value);
+    }
+
+    else if(value == root->data)
+    {
+        cout << "Duplicates not allowed!\n";
+    }
+    return root;
+}
+
+
+// Main Function
+int main()
+{
+    Node *root = NULL;
+    bool run = true;
+
+    do
+    {
+        int ch;
+        cout << "Menu\n1.Insert Values into the Tree\n2.Exit\n";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            int value;
+            cout << "Enter the value to be inserted into the tree : ";
+            cin >> value;
+
+            root = insertBST(root, value);
+            break;
+        
+        case 2:
+            exit(0);
+        
+        default:
+            cout << "Invalid Choice!\n";
+            break;
+        }
+    } while (run == true);
+    
+    return 0;
+}
