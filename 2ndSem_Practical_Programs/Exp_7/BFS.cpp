67a9b9eeaf3463c1f31814707ef39193eda8749d
Haleshot <hari.leo03@gmail.com>
2022-04-20 11:16:34 +0530

    Updated

--- 2ndSem_Practical_Programs/Exp_7/BFS.cpp
+++ 2ndSem_Practical_Programs/Exp_7/BFS.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-void BFS(int, int, int);
+void BFS(**int, int, int);
 
 void Add_Edge(int, int);
 
 
 int main()
 {
-    /*code*/
+    int row, column;
+    cout << "Enter Limit for Height(Row of matrix) of Image : ";
+
+    cin >> row;
+    cout << "Enter Limit for Width(Column of matrix)  of Image : ";
+    cin >> column;
+
+    int **adj = new int *[row];
+    for(int i = 0; i < row; i++)
+    {
+        adj[i] = new int [column];
+    }
+
+    Add_Edge(0, 1);
+    Add_Edge(0, 2);
+    Add_Edge(1, 2);
+    Add_Edge(2, 0);
+    Add_Edge(2, 3);
+    Add_Edge(3, 3);
+
+    BFS(adj, 2, 2);
     return 0;
 }
 
-void BFS(int adj[10][10], int source, int n)
+void BFS(int **array, int source, int n)
 {
     int visited[n];
     queue<int> q;
@@ -30,7 +50,7 @@ void BFS(int adj[10][10], int source, int n)
         q.pop();
         for(int k = 0; k < n; k++)
         {
-            if(adj[source][k] == 1)
+            if(array[source][k] == 1)
             {
                 if(visited[k] == 0)
                 {
@@ -44,6 +64,8 @@ void BFS(int adj[10][10], int source, int n)
 
 void Add_Edge(int v, int value)
 {
-    queue<int> adj;
-    
+    queue<int> *adj;
+    adj = new queue <int>[v];
+    adj[v].push(value);
+
 }
