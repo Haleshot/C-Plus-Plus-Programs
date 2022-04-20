3e4b32515e3309645f64c5b85f08456f865de5e8
Haleshot <hari.leo03@gmail.com>
2022-04-20 13:11:01 +0530

    Updated

--- 2nd_sem_DSA_Project/tempCodeRunnerFile.cpp
+++ 2nd_sem_DSA_Project/tempCodeRunnerFile.cpp
@@ -1,1 +1,19 @@
-\t\t\t\t\t\t
+bool Restaurants::Validity_Of_Coupon(string str, int n)
+{
+    // Checking for Invalid Coupons.
+    if (!((str[0] >= 'a' && str[0] <= 'z')
+          || (str[0] >= 'A' && str[0] <= 'Z')
+          || str[0] == '_'))
+        return false;
+ 
+    for (int i = 1; i < str.length(); i++) {
+        if (!((str[i] >= 'a' && str[i] <= 'z')
+              || (str[i] >= 'A' && str[i] <= 'Z')
+              || (str[i] >= '0' && str[i] <= '9')
+              || str[i] == '_'))
+            return false;
+    }
+ 
+    // String is a coupon
+    return true;
+}
