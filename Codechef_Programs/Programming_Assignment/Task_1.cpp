6d380d78ef3cb6a49184e1bdf4e71cb39217c4a7
Haleshot <hari.leo03@gmail.com>
2022-04-23 20:34:20 +0530

    Updated

--- Codechef_Programs/Programming_Assignment/Task_1.cpp
+++ Codechef_Programs/Programming_Assignment/Task_1.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        long terms_1, terms_2, terms_3;
+        cin >> terms_1;
+        
+
+    }
     return 0;
 }
