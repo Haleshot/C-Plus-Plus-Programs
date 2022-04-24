a62c168769cabb052ee9bbd430d573f97326eae5
Haleshot <hari.leo03@gmail.com>
2022-04-24 10:03:03 +0530

    pdated

--- Codechef_Programs/Programming_Assignment/Task_1.cpp
+++ Codechef_Programs/Programming_Assignment/Task_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <cstring>
 using namespace std;
 
 int main()
@@ -11,45 +12,47 @@ int main()
     {
         long terms_1, terms_2, terms_3;
         int c = 0;
+
         for(int i = 0; i < T; i++)
         {
             cin >> terms_1;
             cin >> terms_2;
             cin >> terms_3;
         }
-        /*if(terms_1 > terms_2)
+        if(terms_1 == terms_2 || terms_1 == terms_3 || terms_2 == terms_3)
         {
-            if(terms_1 > terms_3)
-            {
-                return terms_1;
-            }
-            else
-            {
-                return terms_3;
-            }
+            cout << "no";
         }
-        else if(terms_2 > terms_1)
+        else
         {
-            if(terms_2 > terms_3)
+            string list_1 = to_string(terms_1);
+            string list_2 = to_string(terms_2);
+            string list_3 = to_string(terms_3);
+
+            int n;
+            n = list_1.length();
+
+            char array_1[n + 1], array_2[n], array_3[n];
+
+            strcpy(array_1, list_1.c_str());
+            strcpy(array_2, list_2.c_str());
+            strcpy(array_3, list_3.c_str());
+
+            int Array_1[n], Array_2[n], Array_3[n];
+            for(int i = 0; i < n; i++)
             {
-                return terms_2;
+                Array_1[i] = int(array_1[i]);
             }
-            else
+            for(int i = 0; i < n; i++)
             {
-                return terms_3;
+                Array_2[i] = int(array_2[i]);
             }
+            for(int i = 0; i < n; i++)
+            {
+                Array_3[i] = int(array_3[i]);
+            }
+            
         }
-        else if(terms_3 > terms_1)
-        {
-            if(terms_3 > terms_2)
-            {
-                return terms_3;
-            }
-            else
-            {
-                return terms_2;
-            }
-        }*/
     }
     return 0;
 }
