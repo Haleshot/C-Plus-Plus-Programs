1548c283ee91180f0ddf3d258418383684adf7af
Haleshot <hari.leo03@gmail.com>
2022-04-24 09:26:00 +0530

    Updated

--- Codechef_Programs/Programming_Assignment/Task_1.cpp
+++ Codechef_Programs/Programming_Assignment/Task_1.cpp
@@ -10,20 +10,46 @@ int main()
     while(T--)
     {
         long terms_1, terms_2, terms_3;
+        int c = 0;
         for(int i = 0; i < T; i++)
         {
             cin >> terms_1;
             cin >> terms_2;
             cin >> terms_3;
         }
-        int Array[terms];
-        if(terms_1 >= terms_2)
+        /*if(terms_1 > terms_2)
         {
-            if(terms_1 >= terms_2)
+            if(terms_1 > terms_3)
             {
-                
+                return terms_1;
+            }
+            else
+            {
+                return terms_3;
             }
         }
+        else if(terms_2 > terms_1)
+        {
+            if(terms_2 > terms_3)
+            {
+                return terms_2;
+            }
+            else
+            {
+                return terms_3;
+            }
+        }
+        else if(terms_3 > terms_1)
+        {
+            if(terms_3 > terms_2)
+            {
+                return terms_3;
+            }
+            else
+            {
+                return terms_2;
+            }
+        }*/
     }
     return 0;
 }
