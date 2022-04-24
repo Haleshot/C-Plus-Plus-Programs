aa37e84990f957c15ab79c035946bdd4d7c3ee7e
Haleshot <hari.leo03@gmail.com>
2022-04-24 12:38:41 +0530

    Updated

--- Codechef_Programs/Programming_Assignment/Task_2.cpp
+++ Codechef_Programs/Programming_Assignment/Task_2.cpp
@@ -3,6 +3,49 @@ using namespace std;
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int N, sum = 0;
+        cin >> N;
+        int A[N];
+        int  backlog_counter = 0;
+        float avg;
+        for(int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
+        int Max = A[0], Min = A[0];
+        
+
+        for (int i = 0; i < N; i++)
+        {
+            if(A[i] < 31)
+            {
+                backlog_counter++;
+            } 
+        }
+        for (int i = 0; i < N; i++)
+        {
+            
+            if(A[i] >= 31)
+            {
+                sum += A[i];
+            }
+        }
+        avg = sum/ N;
+
+        for (int i = 0; i < N; i++)
+        {
+            
+            if(A[i] >= 31)
+            {
+                sum += A[i];
+            }
+        }
+        
+
+    }
     return 0;
 }
