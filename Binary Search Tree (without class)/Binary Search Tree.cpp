fc3d56b0a092942699f91ad59cb2d67bbd802dfa
Haleshot <hari.leo03@gmail.com>
2022-07-19 21:55:31 +0530

    Updated

--- Binary Search Tree (without class)/Binary Search Tree.cpp
+++ Binary Search Tree (without class)/Binary Search Tree.cpp
@@ -9,6 +9,7 @@ struct Node
     Node *right;
 };
 
+// Inserting Nodes into the Tree by comparing the value with the source and root nodes.
 Node *Insert_BST(struct Node *root, int value)
 {
     if(root == NULL)
@@ -27,10 +28,31 @@ Node *Insert_BST(struct Node *root, int value)
     {
         root->right = Insert_BST(root->right, value);
     }
+    else if (root->data == value)
+    {
+        cout << "Duplicates not allowed!" << endl;
+    }
+    return root;
+}
+
+// Function to print the Inorder Traversal of the Binary Search Tree
+Node *Inorder(Node *root)
+{
+    if (!(root))
+    {
+        return 0;
+    }
+    Inorder(root->left);
+    cout << root->data << endl;
+    Inorder(root->right);
 }
 
 int main()
 {
-
+    
+    bool run = true;
+    int value_user;
+    cout << "Enter the value to be entered into the BST : " << endl;
+    cin >> value_user;
     return 0;
 }
