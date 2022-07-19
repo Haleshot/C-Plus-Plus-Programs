6e7ffeb0ab66a02a5f8bb7296d83f1240434c237
Haleshot <hari.leo03@gmail.com>
2022-07-19 21:50:11 +0530

    Updated

---
+++ Binary Search Tree (without class)/Binary Search Tree.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace  std;
+
+// Structure for Binary Search Tree
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *Insert_BST(struct Node *root, int value)
+{
+    if(root == NULL)
+    {
+        Node *newnode = new Node;
+        newnode->data = value;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        root = newnode
+    }
+    else if (root->data > value)
+    {
+        root->left = Insert_BST(root->left, value);
+    }
+    else if (root->data < value)
+    {
+        root->right = Insert_BST(root->right, value);
+    }
+}
+
+int main()
+{
+    return 0;
+}
