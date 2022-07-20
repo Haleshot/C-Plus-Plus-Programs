3a079eb6e548c7627cb051224b99718c1731e819
Haleshot <hari.leo03@gmail.com>
2022-07-20 08:52:11 +0530

    Updated

--- Binary_Search/Binary_Search.cpp
+++ Binary_Search/Binary_Search.cpp
@@ -1,3 +1,4 @@
+// Program which helps in finding an element in an array, Binary Search Technique, Complexity = O(log(n))
 #include <iostream>
 using namespace std;
 int Binary_Search(int*, int, int, int);
